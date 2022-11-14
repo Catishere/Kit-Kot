985317d8333e58bef534c83aa34a309a250e7d9b
Catishere <viktor.gyoshev@gmail.com>
2022-11-14 18:02:56 +0200

    remove useless styles

--- client/kitkot/src/views/Following.tsx
+++ client/kitkot/src/views/Following.tsx
@@ -1,9 +1,5 @@
 import { Box } from "@mui/material";
 
 export function Following() {
-  return (
-    <Box sx={{ backgroundColor: "white", width: "100%", height: "100%" }}>
-      <Box>fOllow</Box>
-    </Box>
-  );
+  return <Box>Follow</Box>;
 }
